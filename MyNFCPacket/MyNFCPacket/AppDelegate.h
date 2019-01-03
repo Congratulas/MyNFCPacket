853942881b894bc705a6007a1813fca7239fcaf6
肖夏 <xiaoxia@cjsc.com>
2019-01-03 17:02:06 +0800

    new project

---
+++ MyNFCPacket/MyNFCPacket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyNFCPacket
+//
+//  Created by congratulas on 2019/1/3.
+//  Copyright © 2019 xiaoxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
